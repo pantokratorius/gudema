eca0fb60a033e01990a4dadc5d0021b8b784c3d3
Erik <eitu@mail.ru>
2021-12-25 17:51:10 +0300

    added show 10 in Orders

--- src/components/Shop/PaginationOrders/PaginationOrders.js
+++ src/components/Shop/PaginationOrders/PaginationOrders.js
@@ -38,6 +38,7 @@ const PaginationOrders = ({pages, data,  getOrders, filterParams, page}) => {
         <div className="filter-pagin">
         <span>{t('filters.show')}</span>
             <Form.Select onChange={onChangeHandleOrder.bind(this,data)}>
+                <option value="10">10</option>
                 <option value="20">20</option>
                 <option value="50">50</option>
                 <option value="100">100</option>
