33f5a8da9be22ce347b25895501605a0fe3837d1
Erik <eitu@mail.ru>
2022-04-09 16:17:04 +0300

    making balance

--- src/components/Shop/PaginationBalances/PaginationBalances.js
+++ src/components/Shop/PaginationBalances/PaginationBalances.js
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 
 
 
-const PaginationBalances = ({pages, data,  getBalances, filterParams}) => {
+const PaginationBalances = ({pages, data,  getBalance, filterParams}) => {
     
   const { t } = useTranslation();
     
